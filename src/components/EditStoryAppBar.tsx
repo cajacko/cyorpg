7f90e5438b9031c261f0a9071ee08a66069a674f
Charlie Jackson <charlie@charliejackson.com>
2019-05-31 17:15:35 +0100

    ✨ feat(add-story): Only show save when there are changes to save
    - Original Branch: master

--- src/components/EditStoryAppBar.tsx
+++ src/components/EditStoryAppBar.tsx
@@ -4,30 +4,46 @@ import { connect } from 'react-redux';
 import { saveStory } from '../store/storiesById/actions';
 import * as AppBar from '../context/AppBar';
 import { Dispatch } from '../store/types';
+import { AppState } from '../store';
 
 type RouteProps = RouteComponentProps<{
   storyId: string;
 }>;
 
+interface IStateProps {
+  hasChanges: boolean;
+}
+
 interface IDispatchProps {
   save: () => void;
 }
 
-interface IProps extends RouteProps, IDispatchProps {}
+interface IProps extends RouteProps, IDispatchProps, IStateProps {}
 
 /**
  * Add a story
  */
-const AddStory: React.FC<IProps> = ({ save }: IProps) => (
+const AddStory: React.FC<IProps> = ({ save, hasChanges }: IProps) => (
   <AppBar.Consumer
     title="Edit Story"
     leftButton="BACK"
-    rightButton={{
-      text: 'Save',
-      action: save,
-    }}
+    rightButton={
+      hasChanges
+        ? {
+          text: 'Save',
+          action: save,
+        }
+        : undefined
+    }
   />
 );
+
+/**
+ * Grab props from the state
+ */
+const mapStateToProps = (state: AppState, props: RouteProps): IStateProps => ({
+  hasChanges: !!state.editedStoriesById[props.match.params.storyId],
+});
 
 /**
  * Map the save draft story prop to the component
@@ -45,7 +61,7 @@ const mapDispatchToProps = (
 
 export default withRouter(
   connect(
-    undefined,
+    mapStateToProps,
     mapDispatchToProps
   )(AddStory)
 );
