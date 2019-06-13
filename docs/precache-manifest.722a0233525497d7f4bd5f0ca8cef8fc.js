086536cf668ceff3abec9d870f459daa722f5b45
Charlie Jackson <charlie@charliejackson.com>
2019-06-13 20:58:32 +0100

    📝 docs(emotions): Add the docs
    - Original Branch: emotions

---
+++ docs/precache-manifest.722a0233525497d7f4bd5f0ca8cef8fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a8326e51802dea0d90b9b19ea3e994",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb70b4c4dfd06ca21ce5",
+    "url": "/static/js/2.3aeea929.chunk.js"
+  },
+  {
+    "revision": "c14582ac5d3e3f558e97",
+    "url": "/static/js/main.6a744662.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
